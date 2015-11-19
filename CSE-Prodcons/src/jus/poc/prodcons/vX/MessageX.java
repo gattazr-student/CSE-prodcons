a6a83250217757b88c0da733c420a3e7ee89dbac
Rémi GATTAZ <remi.gattaz@gmail.com>
2015-11-19 23:46:35 +0100

    Create hierarchy of classes

---
+++ CSE-Prodcons/src/jus/poc/prodcons/vX/MessageX.java
@@ -0,0 +1,7 @@
+package jus.poc.prodcons.vX;
+
+import jus.poc.prodcons.Message;
+
+public class MessageX implements Message {
+
+}
---
+++ CSE-Prodcons/src/jus/poc/prodcons/vX/TestProdCons.java
@@ -0,0 +1,28 @@
+package jus.poc.prodcons.vX;
+
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
+
+public class TestProdCons extends Simulateur {
+
+	public TestProdCons(Observateur aObservateur) {
+		super(aObservateur);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void run() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Entry point
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new TestProdCons(new Observateur()).start();
+	}
+
+}
