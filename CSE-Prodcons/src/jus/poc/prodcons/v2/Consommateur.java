6a9d664a8ee381db52a5ce78df446358f5627030
Quentin Dunand <quentindunand@gmail.com>
2015-12-10 17:57:57 +0100

    Package v2 created

---
+++ CSE-Prodcons/src/jus/poc/prodcons/v2/Consommateur.java
@@ -0,0 +1,28 @@
+package jus.poc.prodcons.v2;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Consommateur;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	protected Consommateur(Observateur aObservateur,
+			int aMoyenneTempsDeTraitement, int aDeviationTempsDeTraitement)
+					throws ControlException {
+		super(Acteur.typeConsommateur, aObservateur, aMoyenneTempsDeTraitement,
+				aDeviationTempsDeTraitement);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ CSE-Prodcons/src/jus/poc/prodcons/v2/ProdCons.java
@@ -0,0 +1,35 @@
+package jus.poc.prodcons.v2;
+
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Tampon;
+import jus.poc.prodcons._Consommateur;
+import jus.poc.prodcons._Producteur;
+
+public class ProdCons implements Tampon {
+
+	@Override
+	public int enAttente() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public synchronized Message get(_Consommateur aConsommateur)
+			throws Exception, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public synchronized void put(_Producteur aProducteur, Message aMessage)
+			throws Exception, InterruptedException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public int taille() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
